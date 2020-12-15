a87e80b83c9b625c105ef26e99ed244d3fb6e513
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2020-12-15 11:36:34 -0300

    formGroup para formulário

--- src/pages/cadastrar-conta/cadastrar-conta.ts
+++ src/pages/cadastrar-conta/cadastrar-conta.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, RequiredValidator, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
@@ -9,7 +10,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CadastrarContaPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup : FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+      this.formGroup = this.formBuilder.group({
+        nome: ['',[Validators.required]],
+        valor:['',[Validators.required]],
+        empresa: [null,[Validators.required]],
+        tipoCusto: ['2',[Validators.required]],
+        dataVencimento: [null,[Validators.required]],
+        estadoPagamento: ['2',[]],
+        vigencia: [null,[]]
+      });
   }
 
   ionViewDidLoad() {
